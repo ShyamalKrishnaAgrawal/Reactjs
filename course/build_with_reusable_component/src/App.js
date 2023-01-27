e6d249a54820d23f199846b15af33fc6f93b4676
Shyamal K <shyamalka@apra.in>
2023-01-27 20:21:50 +0530

    Section 3 file (build_with_reusable_component) change.

--- course/build_with_reusable_component/src/App.js
+++ course/build_with_reusable_component/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import ProfileCard from "./Profilecard"
-
+import Alexa from "./images/alexa.png"
+import Cortana from "./images/cortana.png"
+import Siri from "./images/siri.png"
+import 'bulma/css/bulma.css'
 function App() {
   return (
     <div>
       <div>Peronal Digital Assitant</div>
-      <ProfileCard title="Alexa" handle="@alexa99"/>
-      <ProfileCard title="Cortana" handle="@cortana32"/>
-      <ProfileCard title="Siri" handle="@siri01"/>
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-4'>
+              <ProfileCard title="Alexa" handle="@alexa99" image={Alexa}/>
+            </div>
+            <div className='column is-4'>
+              <ProfileCard title="Cortana" handle="@cortana32" image={Cortana}/>
+            </div>
+            <div className='column is-4'>
+              <ProfileCard title="Siri" handle="@siri01" image={Siri}/>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- course/build_with_reusable_component/src/Profilecard.js
+++ course/build_with_reusable_component/src/Profilecard.js
@@ -3,8 +3,19 @@ import React from 'react'
 function Profilecard(props) {
   return (
     <div>
-      <div>Title is {props.title}</div>
-      <div>Handle is {props.handle}</div>
+        <div className='card'>
+            <div className='card-image'>
+                <figure className='image is-bly'>
+                <img src={props.image} alt="pda logo"/>
+                </figure>
+            </div>
+        </div>
+        <div className='card-conten'>
+            <div className='media-content'>
+                <p className='tittle is 4'>Title is {props.title}</p>
+                <p className='subtitle is-6'>Handle is {props.handle}</p>
+            </div>
+        </div>
     </div>
   )
 }
